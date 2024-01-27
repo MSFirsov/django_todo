24b814207737e884a1e7f07ffbdad0ef265c6925
MSFirsov <mxfrsv@MacBook.local>
2024-01-27 20:57:34 +0300

    bug fix

--- todo/tasklist/urls.py
+++ todo/tasklist/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('<int:week_num>/', views.index, name='home_with_week'),
-    path('create_task/<int:week_num>/<slug:task_date>/', views.create_task, name='create_task'),
-    path('delete_task/<int:week_num>/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('is_completed_task/<int:week_num>/<int:task_id>/', views.is_completed_task, name='is_completed_task'),
+    path('<int:cur_year>/<int:week_num>/', views.index, name='home_with_week'),
+    path('create_task/<int:cur_year>/<int:week_num>/<slug:task_date>/', views.create_task, name='create_task'),
+    path('delete_task/<int:cur_year>/<int:week_num>/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('is_completed_task/<int:cur_year>/<int:week_num>/<int:task_id>/', views.is_completed_task, name='is_completed_task'),
 
 ]
--- todo/tasklist/views.py
+++ todo/tasklist/views.py
@@ -1,11 +1,8 @@
 import datetime
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
-from django.views.generic import CreateView
+from django.http import HttpResponseNotFound
+from django.shortcuts import render, redirect
 
 from .forms import CreateTaskForm
 from .models import Task
@@ -16,17 +13,15 @@ iso_date = datetime.date.today().isocalendar()
 
 
 @login_required
-def index(request, week_num=iso_date[1]):
+def index(request, cur_year=iso_date[0], week_num=iso_date[1]):
+    if week_num >= 53:
+        cur_year += 1
+        week_num = 1
+    elif week_num <= 0:
+        cur_year -= 1
+        week_num = 52
 
-    day_list = [
-        datetime.date.fromisocalendar(iso_date[0], int(week_num), 1),
-        datetime.date.fromisocalendar(iso_date[0], int(week_num), 2),
-        datetime.date.fromisocalendar(iso_date[0], int(week_num), 3),
-        datetime.date.fromisocalendar(iso_date[0], int(week_num), 4),
-        datetime.date.fromisocalendar(iso_date[0], int(week_num), 5),
-        datetime.date.fromisocalendar(iso_date[0], int(week_num), 6),
-        datetime.date.fromisocalendar(iso_date[0], int(week_num), 7)
-    ]
+    day_list = [datetime.date.fromisocalendar(cur_year, int(week_num), x) for x in range(1, 8)]
 
     task_list = Task.objects.filter(user_id=request.user).all()
 
@@ -36,25 +31,23 @@ def index(request, week_num=iso_date[1]):
         'task_list': task_list,
         'week_num': week_num,
         'day_today': day_today,
+        'cur_year': cur_year,
 
     }
     return render(request, 'tasklist/index.html', context=data)
 
 
 @login_required
-def create_task(request, week_num, task_date):
+def create_task(request, cur_year, week_num, task_date):
     if request.method == 'POST':
         new_post = dict(**request.POST)
         new_post['task_date'] = datetime.datetime.strptime(task_date, '%Y-%m-%d').date()
         new_post['user'] = request.user.id
         new_post['text'] = new_post['text'][0]
-        new_post['csrfmiddlewaretoken'] = new_post['csrfmiddlewaretoken'][0]
         form = CreateTaskForm(new_post)
         if form.is_valid():
-            # print(request.POST)
-            # print(new_post)
             form.save()
-            return redirect('home_with_week', week_num=week_num)
+            return redirect('home_with_week', cur_year=cur_year, week_num=week_num)
     else:
         form = CreateTaskForm()
 
@@ -63,28 +56,30 @@ def create_task(request, week_num, task_date):
         'week_num': week_num,
         'task_date': task_date,
         'form': form,
+        'cur_year': cur_year,
+
     }
     return render(request, 'tasklist/create_task.html', context=data)
 
 
 @login_required
-def delete_task(request, week_num, task_id):
+def delete_task(request, cur_year, week_num, task_id):
     task = Task.objects.get(pk=task_id)
     if task is None:
         pass
 
     task.delete()
-    return redirect('home_with_week', week_num=week_num)
+    return redirect('home_with_week', cur_year=cur_year, week_num=week_num)
 
 
-def is_completed_task(request, week_num, task_id):
+def is_completed_task(request, cur_year, week_num, task_id):
     task = Task.objects.get(pk=task_id)
     if task.is_completed == 0:
         task.is_completed = 1
     else:
         task.is_completed = 0
     task.save()
-    return redirect('home_with_week', week_num=week_num)
+    return redirect('home_with_week', cur_year=cur_year, week_num=week_num)
 
 
 def page_not_found(request, exception):
