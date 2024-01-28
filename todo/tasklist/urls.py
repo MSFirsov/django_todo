2d06ef0f4ea73792edbea22e8e6e20a00c25bfde
MSFirsov <mxfrsv@gmail.com>
2024-01-28 17:36:59 +0300

    correction

--- todo/tasklist/urls.py
+++ todo/tasklist/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path, register_converter
+from django.urls import path
 from . import views
 
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:cur_year>/<int:week_num>/', views.index, name='home_with_week'),
     path('create_task/<int:cur_year>/<int:week_num>/<slug:task_date>/', views.create_task, name='create_task'),
     path('delete_task/<int:cur_year>/<int:week_num>/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('is_completed_task/<int:cur_year>/<int:week_num>/<int:task_id>/', views.is_completed_task, name='is_completed_task'),
+    path('is_completed_task/<int:cur_year>/<int:week_num>/<int:task_id>/',
+         views.is_completed_task, name='is_completed_task'),
 
 ]
